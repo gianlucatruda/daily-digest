9e7a1e25ec82964139427d843eb413dddf5a2d5a
Gianluca Truda <gianlucatruda@gmail.com>
2020-03-20 15:11:18 +0200

    Added dynamic caching for clips.json. Allowed page regresh to regenerate new random samples.

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, url_for
-from digest_utils import clips_to_html
+import digest_utils
+from loguru import logger
 
 app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    TEMPLATES_AUTO_RELOAD=True,
+)
+logger.debug(f'Flask config:\n{app.config}')
 
 @app.route('/', methods=['GET'])
 def kindle_digest(name=None):
+    kindle_notes = digest_utils.get_kindle_clips(use_cache=True)
+    # digest_utils.cache_kindle_clips(kindle_notes)
+    digest_utils.clips_to_html(path='templates/index.html', clips=kindle_notes)
+
     url_for('static', filename='style.css')
     return render_template('index.html', name=name)
--- digest_utils.py
+++ digest_utils.py
@@ -1,17 +1,9 @@
 import requests
 import random
 import json
+import os
 from secrets import TOKEN
-
-
-def random_clips(clips, num=5):
-    options = generate_options(clips)
-    choices = []
-    for _ in range(num):
-        choice = random.choice(options)
-        choices.append(choice)
-        options.remove(choice)
-    return choices
+from loguru import logger
 
 
 def generate_options(clips) -> list:
@@ -25,6 +17,17 @@ def generate_options(clips) -> list:
     return options
 
 
+def random_clips(clips, num=5):
+    logger.debug(f'Generating {num} random clips...')
+    options = generate_options(clips)
+    choices = []
+    for _ in range(num):
+        choice = random.choice(options)
+        choices.append(choice)
+        options.remove(choice)
+    return choices
+
+
 def clips_to_html(path='index.html', *args, **kwargs):
     clips = random_clips(*args, **kwargs)
     out = '<link rel="stylesheet" type="text/css" href="static/style.css" media="screen"/>'
@@ -33,13 +36,26 @@ def clips_to_html(path='index.html', *args, **kwargs):
         f.write(out)
 
 
-def get_kindle_clips(token=TOKEN):
+def get_kindle_clips(token=TOKEN, use_cache=False, cache_path='cache/clips.json'):
+    if use_cache:
+        if os.path.exists(cache_path):
+            with open(cache_path, 'r') as file:
+                clips = json.load(file)
+            logger.info(f'Using cached clips at {cache_path}')
+            return clips
+        else:
+            logger.warning(f'Tried retrieving cache, but {cache_path} could not be found.')
     url = f'https://raw.githubusercontent.com/gianlucatruda/kindle-reading-notes/master/clips.json?token={TOKEN}'
+    logger.info(f'Retrieving clips from {url}')
     req = requests.get(url)
-    print(req)
+    logger.debug(f'Request: {req}')
+    if use_cache:
+        cache_kindle_clips(req.json(), path=cache_path)
     return req.json()
 
+
 def cache_kindle_clips(clips, path='cache/clips.json'):
+    logger.debug(f'Writing clips to cache at {path}...')
     with open(path, 'w') as file:
         json.dump(clips, file)
 
